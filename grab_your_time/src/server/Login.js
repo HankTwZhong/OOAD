8735dc4203a5271c159555112022ab10e7abbd3d
DESKTOP-4V64E9U\cmf15bs <cmf15bw@gmail.com>
2018-05-06 13:19:12 +0800

    commit server js file

---
+++ grab_your_time/src/server/Login.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var connection = mongoose.connect('mongodb://localhost/timelog')
+
+var db = mongoose.connection;
+
+class Login {
+    login(account, password){
+        return new Promise((resolve, rejeect) => {
+
+        })
+    }
+}
+
+module.exports = new Login();
