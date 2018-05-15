e18715e7ce301a1871c88c762495e6cf9145d50c
DESKTOP-4V64E9U\cmf15bs <cmf15bw@gmail.com>
2018-05-15 12:41:35 +0800

    modify ppt and implement CalculateTotialTime

--- grab_your_time/src/server/ChartInformation.js
+++ grab_your_time/src/server/ChartInformation.js
@@ -3,9 +3,14 @@ class ChartInformation {
         this.chartInformationList ;
     }
 
-    CalculateTotialTime(typeList){
+    CalculateTotialTime(typeList, startDate, endDate){
         this.chartInformationList = typeList;
-        return this.chartInformationList
+        // let measureHour = (this.chartInformationList.eventList[0].end - this.chartInformationList.eventList[0].start)/3600000 ;
+        this.chartInformationList.eventList.forEach((event)=>{
+            event.countTime = (event.end - event.start) / 3600000;
+            console.log(event.countTime);
+        })
+        return this.chartInformationList;
     }
 }
 
--- grab_your_time/src/test/chartinformationTest.js
+++ grab_your_time/src/test/chartinformationTest.js
@@ -1,21 +1,28 @@
 let Type = require('../server/Type');
+let Event = require('../server/Event');
 
 let ChartInformation = require('../server/ChartInformation')
-let expectTypeList = [{"eventList": [],"typeName": "STV"}]
+let expectTypeList = {"eventList": [],"typeName": "STV"}
 let eventList =[{
-    title: 'OOAD',
-    start: new Date("1995-12-17T03:24:00"),
-    end: {
-        type: Date,
-        require:true
-    },
-    desc: {
-        type: String
-    }
+    title: 'STV',
+    start: new Date("1995-12-17T12:00:00"),
+    end: new Date("1995-12-17T18:00:00"),
+    desc: 'coding'
+},{
+    title: 'STV',
+    start: new Date("1995-12-17T13:00:00"),
+    end:new Date("1995-12-17T15:00:00"),
+    desc: 'hPTH'
 }]
 
 
-expectTypeList.eventList.push(new Event());
+
+let eve = new Event(eventList[0].title, eventList[0].start, eventList[0].end, eventList[0].desc);
+let eve2 = new Event(eventList[1].title, eventList[1].start, eventList[1].end, eventList[1].desc);
+
+expectTypeList.eventList.push(eve);
+expectTypeList.eventList.push(eve2)
+console.log(expectTypeList);
 let char = new ChartInformation();
 let result = char.CalculateTotialTime(expectTypeList);
-console.log(result);
+console.log("result:\t"+JSON.stringify(result));
